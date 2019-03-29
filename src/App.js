8c976cd14c4fb1918e15c78189cdcb5582ae8608
Linead96 <danielgarboza@yahoo.com>
2019-03-29 17:53:40 +0800

    added 3 different ways to create python text editor in the browser using React

--- src/App.js
+++ src/App.js
@@ -1,28 +1,75 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import Editor from 'react-simple-code-editor';
+import Script from 'react-load-script';
+import { highlight, languages } from 'prismjs/components/prism-core';
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-javascript';
 import './App.css';
+import './css/prism.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+const code = `function add(a, b) {
+  return a + b;
+}
+`;
+
+const styles = {
+  textarea: {
+    height: '300px',
+    width: '300px',
+  }
+}
+ 
+class App extends React.Component {
+  state = {
+    code,
+    output: "",
+  };
+ 
+render() {
+  return (
+    <div>
+      <Script src="./utils/brython.js" />
+      
+      <Editor
+        className="language-python"
+        value={this.state.code}
+        onValueChange={code => this.setState({ code })}
+        highlight={code => highlight(code, languages.js)}
+        padding={10}
+        style={{
+          fontFamily: '"Fira code", "Fira Mono", monospace',
+          fontSize: 12,
+        }}
+      />
+      <button>Run</button>
+      <textarea style={styles.textarea}>
+        {this.state.output}
+      </textarea>
+      
+      <iframe
+        title="python-code-editor"
+        src="https://trinket.io/embed/python3/e3366b7a37"
+        width="100%" height="356"
+        frameborder="0"
+        marginwidth="0"
+        marginheight="0"
+        allowfullscreen>
+      </iframe>
+
+      <iframe
+        title="python-console"
+        src="http://brython.info/console.html"
+        width="800"
+        height="400"
+      ></iframe>
+
+    </div>
+    
+      
     );
   }
 }
+
+
 
 export default App;
